2c5aec35359c50c221f659f9ee59f4340ed0fb82
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-04-17 16:04:01 +0400

    added solveEquation function

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = Math.pow(b, 2) - 4 * a * c;
+
+  if (D < 0) {
+    return arr;
+  }
+
+  if (D == 0) {
+    arr.push(-b / (2 * a));
+  }
+
+  if (D > 0) {
+    arr.push((-b + Math.sqrt(D)) / (2 * a));
+    arr.push((-b - Math.sqrt(D)) / (2 * a));
+  }
+
   return arr;
 }
 
