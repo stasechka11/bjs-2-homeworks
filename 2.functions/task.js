249e8bbc6805dda3959ae5cc564a3e3ba094dc50
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-04-19 11:22:53 +0400

    added getArrayParams function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,21 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[arr.length - 1];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= max) {
+      max = arr[i];
+    }
+
+    if (arr[i] <= min) {
+      min = arr[i];
+    }
+
+    sum += arr[i];
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
