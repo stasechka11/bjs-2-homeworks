cd145bd2c01e3be589ce100dabf4be9549285c19
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-04-22 17:46:30 +0400

    Added makeWork function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -71,5 +71,15 @@ function averageEvenElementsWorker(...arr) {
 }
 
 function makeWork (arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
 
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const result = func(...arrOfArr[i]);
+
+    if (result > maxWorkerResult) {
+      maxWorkerResult = result;
+    }
+  }
+
+  return maxWorkerResult;
 }
