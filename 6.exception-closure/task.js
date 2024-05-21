01f83daae07328aba1ac918e9335c1cefe7f051b
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-05-21 11:06:43 +0400

    added functions parseCount and validateCount

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    if (parsedValue === NaN) {
+        throw new Error("Невалидное значение");
+    }
+
+    return parsedValue;
+} 
+
+function validateCount(value) {
+    try{
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
